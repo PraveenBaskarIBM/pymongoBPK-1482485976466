2096b8935c628dc1e3e4e754258251f99992b8cb
Praveen Kumar B <pbkumar2@in.ibm.com>
2016-12-23 05:17:53 -0600

    codeC

--- server.py
+++ server.py
@@ -30,27 +30,27 @@ print('connected to mongodb!, welcome to mongodb connection, have a fun')
 import pandas as pd
 import cPickle as pickle
 
-data = pd.read_csv('d1_test.csv', header=None, names=['col1', 'col2', 'col3', 'col4', 'col5', 'col6'])
+data = pd.read_csv('d1_test.csv', header=None, names=['Col1', 'Col2', 'Col3', 'Col4', 'Col5', 'Col6'])
 print data
 print('\n')
 with open('test3_model.pkl', 'rb') as f:
   classifier = pickle.load(f)
 
-del data['col1']
+del data['Col1']
 predicted_set = classifier.predict(data)
 prob_predicted = classifier.predict_proba(data)
 
-data = pd.DataFrame(data, columns=["col1", "col2", "col3", "col4", "col5", "col6"])
-pred = pd.DataFrame(predicted_set, columns=["col7"])
-df_prob = pd.DataFrame(prob_predicted, columns=["col8", "col9"])
+data = pd.DataFrame(data, columns=["Col1", "Col2", "Col3", "Col4", "Col5", "Col6"])
+pred = pd.DataFrame(predicted_set, columns=["Col7"])
+df_prob = pd.DataFrame(prob_predicted, columns=["Col8", "col9"])
 
 frame1 = [data,pred]
 df1 = pd.concat(frame1,axis=1, join_axes=[data.index])
 frame2 = [df1,df_prob]
 df2 = pd.concat(frame2,axis=1, join_axes=[data.index])
 
-df2['col10'] = df2['col9'].map(lambda x: 'Low' if x < 0.5 else 'Medium' if x < 0.75 else 'High')
-del df2['col1']
+df2['Col10'] = df2['Col9'].map(lambda x: 'Low' if x < 0.5 else 'Medium' if x < 0.75 else 'High')
+del df2['Col1']
 print df2
 
 
