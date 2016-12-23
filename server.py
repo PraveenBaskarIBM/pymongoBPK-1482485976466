a5e4c2d3d2cd9bb6df9c6be14591cf47e787b19e
Praveen Kumar B <pbkumar2@in.ibm.com>
2016-12-23 03:42:33 -0600

    new

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
 import os
+import pymongo
+#import ssl
+import json  
+from pymongo import MongoClient
 try:
   from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
   from SocketServer import TCPServer as Server
@@ -11,6 +15,19 @@ PORT = int(os.getenv('PORT', 8000))
 # Change current directory to avoid exposure of control files
 os.chdir('static')
 
+# VCAP_SERVICES mapping Start
+
+services = os.getenv('VCAP_SERVICES')
+services_json = json.loads(services)
+mongodb_url = services_json['compose-for-mongodb'][0]['credentials']['uri']
+#connect:
+client = MongoClient(mongodb_url)  
+#get the default database:
+db = client.get_default_database()  
+print('connected to mongodb!, welcome to mongodb connection, have a fun') 
+
+# VCAP_SERVICES mapping END
+
 httpd = Server(("", PORT), Handler)
 try:
   print("Start serving at port %i" % PORT)
@@ -18,4 +35,5 @@ try:
 except KeyboardInterrupt:
   pass
 httpd.server_close()
+
 
