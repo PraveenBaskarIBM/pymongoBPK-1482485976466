536a7c974d1fac57edbc8aabedb62c56d667789a
Praveen Kumar B <pbkumar2@in.ibm.com>
2017-01-04 04:39:44 -0600

    jan4-4

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ import os
 import pymongo
 #import ssl
 import json  
-import getopt
 from pymongo import MongoClient
 try:
   from SimpleHTTPServer import SimpleHTTPRequestHandler as Handler
@@ -31,31 +30,22 @@ print("----------------------------------")
 
 ###----Regression Code begins------###
 import pandas as pd
-import cPickle as pickle
-import sklearn
+#import cPickle as pickle
+#import sklearn
 
-data = pd.read_csv('d1_test.csv', header=None, names=['col1', 'col2', 'col3', 'col4', 'col5', 'col6'])
-print data
-print('\n')
-with open('test3_model.pkl', 'rb') as f:
-  classifier = pickle.load(f)
+#CSV to JSON Conversion
+csvfile = open('d1_test.csv', 'r')
+reader = csv.DictReader(csvfile)
+db.sourcebpk.drop()
 
-del data['col1']
-predicted_set = classifier.predict(data)
-prob_predicted = classifier.predict_proba(data)
+header= [ "col1","col2","col3","col4","col5","col6"]
 
-data = pd.DataFrame(data, columns=["col1", "col2", "col3", "col4", "col5", "col6"])
-pred = pd.DataFrame(predicted_set, columns=["col7"])
-df_prob = pd.DataFrame(prob_predicted, columns=["col8", "col9"])
+for each in reader:
+    row={}
+    for field in header:
+        row[field]=each[field]
 
-frame1 = [data,pred]
-df1 = pd.concat(frame1,axis=1, join_axes=[data.index])
-frame2 = [df1,df_prob]
-df2 = pd.concat(frame2,axis=1, join_axes=[data.index])
-
-df2['col10'] = df2['col9'].map(lambda x: 'Low' if x < 0.5 else 'Medium' if x < 0.75 else 'High')
-del df2['col1']
-print df2
+db.sourcebpk.insert(row)
 
 
 ###----Regression Code Ends------###
